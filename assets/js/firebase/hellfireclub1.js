3b23a5f51eb6d1868eba1123a36d735f6dbff002
Daniel Hofer <111920794+hoferdghg@users.noreply.github.com>
2022-08-26 15:42:08 -0300

    26

--- assets/js/firebase/hellfireclub1.js
+++ assets/js/firebase/hellfireclub1.js
@@ -1,9 +1,8 @@
-import app from "./firebase/app.js"
+import app from "./app.js"
 import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'
 
 
 export async function subscribeToHellfireClub(subscription) {
-    debugger
     const db = getFirestore(app)
     const hellfireClubCollection = collection(db, hellfire-club)
     const docRef = await addDoc (hellfireClubCollection, subscription)
