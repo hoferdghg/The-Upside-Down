52b0611c04319ffc13964f0c146d941db141068e
Daniel Hofer <111920794+hoferdghg@users.noreply.github.com>
2022-08-26 13:53:16 -0300

    tome

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,7 @@
 const txtName = document.getElementById('txtName')
-
-debugger
-
-console.log(txtName)
 const txtEmail = document.getElementById('txtEmail')
 const txtLevel = document.getElementById('txtLevel')
 const txtCharacter = document.getElementById('txtCharacter')
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+debugger
