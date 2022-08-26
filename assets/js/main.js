7f11aea50f4f10e929c3950672f21950b5a7294b
Daniel Hofer <111920794+hoferdghg@users.noreply.github.com>
2022-08-26 13:46:46 -0300

    receba

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-console.log('Passou por aqui!');
+const txtName = document.getElementById('txtName')
+
+debugger
+
+console.log(txtName)
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
